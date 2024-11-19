4de2aa55c653487989d88d79f5c8fb7a7e9f8d3f
Jean Paul Fernandez <jpxoi@icloud.com>
2024-11-19 16:40:57 +0000

    Integrate sidebar components into the layout; wrap children with SidebarProvider

---
+++ components/custom/app-sidebar.tsx
@@ -0,0 +1,68 @@
+import { Home, Landmark, Cake, FileClock, User } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import Link from "next/link"
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "/dashboard",
+    icon: Home,
+  },
+  {
+    title: "Employees",
+    url: "/employee/list",
+    icon: User,
+  },
+  {
+    title: "Payroll",
+    url: "#",
+    icon: Landmark,
+  },
+  {
+    title: "Birthdays",
+    url: "#",
+    icon: Cake,
+  },
+  {
+    title: "Logs",
+    url: "#",
+    icon: FileClock,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Kilburnazon Dashboard</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
