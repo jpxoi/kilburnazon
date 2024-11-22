fae7e47e346b643c3b8f4085a3d6a26a1a247cac
Jean Paul Fernandez <jpxoi@icloud.com>
2024-11-22 15:32:04 +0000

    Add fetch functions for locations, job roles, departments, and employees

---
+++ lib/fetchers.ts
@@ -0,0 +1,66 @@
+import { DepartmentModel, EmployeeAPIResponse, JobRoleModel, LocationModel } from "@/interfaces";
+
+export async function fetchLocations() {
+  const res = await fetch("http://localhost:8000/api/location", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message);
+  }
+
+  return (await res.json()) as LocationModel[];
+}
+
+export async function fetchJobRoles() {
+  const res = await fetch("http://localhost:8000/api/job-role", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message);
+  }
+
+  return (await res.json()) as JobRoleModel[];
+}
+
+export async function fetchDepartments() {
+  const res = await fetch("http://localhost:8000/api/department", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message);
+  }
+
+  return (await res.json()) as DepartmentModel[];
+}
+
+export async function fetchEmployees() {
+  const res = await fetch("http://localhost:8000/api/employee", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message);
+  }
+
+  return (await res.json()) as EmployeeAPIResponse[];
+}
+
+export async function fetchEmployee(id: string) {
+  const res = await fetch(`http://localhost:8000/api/employee/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    const error = await res.json();
+    throw new Error(error.message);
+  }
+
+  return (await res.json()) as EmployeeAPIResponse;
+}
