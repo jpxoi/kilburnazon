5addece71dd0ae02e6ae592012850b17e1310a3f
Jean Paul Fernandez <jpxoi@icloud.com>
2024-11-26 17:35:44 +0000

    Replace LogTerminationPage component with a new implementation for employee termination logs

---
+++ app/(protected)/termination/logs/page.tsx
@@ -0,0 +1,12 @@
+export default function LogTerminationPage() {
+  return (
+    <div className="flex flex-col items-center justify-start min-h-screen px-8 pt-2 pb-20 gap-4 w-full">
+      <div className="flex items-center justify-between w-full gap-2">
+        <h1 className="text-2xl font-bold">Employee Termination Logs</h1>
+      </div>
+      <div className="flex items-center justify-center w-full h-full gap-4">
+        <p className="text-red-900">This is the termination page.</p>
+      </div>
+    </div>
+  );
+}
