90e2b979cf6412f3fc3df45a8aa7a081131e5738
Jean Paul Fernandez <jpxoi@icloud.com>
2024-12-01 20:08:31 +0000

    Add actions column with dropdown menu for log management

--- components/custom/logs-table/columns.tsx
+++ components/custom/logs-table/columns.tsx
@@ -1,12 +1,24 @@
-"use client"
+"use client";
 
-import { TerminationLogAPIResponse } from "@/interfaces"
-import { ColumnDef } from "@tanstack/react-table"
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { TerminationLogAPIResponse } from "@/interfaces";
+import { ColumnDef } from "@tanstack/react-table";
+import { TrashIcon, MoreHorizontal } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export const columns: ColumnDef<TerminationLogAPIResponse>[] = [
   {
     accessorKey: "id",
     header: "Log ID",
+    cell: ({ row }) => <Badge>{row.original.id}</Badge>,
   },
   {
     accessorKey: "employee.name",
@@ -35,5 +47,26 @@ export const columns: ColumnDef<TerminationLogAPIResponse>[] = [
   {
     accessorKey: "retention_timestamp",
     header: "Scheduled Delete Date",
-  }
-]
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem onClick={() => console.log("Delete Inmediatly")}>
+              Delete Inmediatly
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
