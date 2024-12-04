7c570657b244dc61faabf08aefc24fea97994940
Jean Paul Fernandez <jpxoi@icloud.com>
2024-12-04 15:41:04 +0000

    Refactor LeaveRequestsPage to use LeaveRequestsTableContainer with Suspense for loading state

--- app/(protected)/leave-requests/page.tsx
+++ app/(protected)/leave-requests/page.tsx
@@ -1,20 +1,18 @@
-import { DataTable } from "@/components/custom/leave-requests-table/data-table";
+import { Suspense } from "react";
+import LeaveRequestsTableContainer from "@/components/leave-requests-table-container";
+import { DataTableSkeleton } from "@/components/skeleton/data-table-skeleton";
 import { columns } from "@/components/custom/leave-requests-table/columns";
-import { fetchLeaveRequests } from "@/lib/fetchers";
 
-export default async function LeaveRequestsPage() {
-  const leaveRequests = await fetchLeaveRequests().catch((err) => {
-    console.error(err);
-    return [];
-  });
-
+export default function LeaveRequestsPage() {
   return (
     <div className="flex flex-col items-center justify-start min-h-screen px-8 pt-2 pb-20 gap-4 w-full">
       <div className="flex items-center justify-between w-full gap-2">
         <h1 className="text-2xl font-bold">Employee Leave Requests</h1>
       </div>
-      <div className="flex items-center justify-start w-full h-full gap-4">
-        <DataTable columns={columns} data={leaveRequests} />
+      <div className="flex items-center justify-center w-full h-full gap-4">
+        <Suspense fallback={<DataTableSkeleton columns={columns} data={[]} />}>
+          <LeaveRequestsTableContainer />
+        </Suspense>
       </div>
     </div>
   );
