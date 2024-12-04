c57e2d9e957faffc5d352cd5f17bfbafcb70fa52
Jean Paul Fernandez <jpxoi@icloud.com>
2024-12-04 16:44:16 +0000

    Add absenteeism report section with period selection and data fetching

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,30 +1,44 @@
+import AbsenteeismPeriodSelect from "@/components/custom/absenteeism-period-select";
+import AbsenteeismReportTableContainer from "@/components/custom/absenteeism-report-table-container";
 import { columns } from "@/components/custom/absenteeism-report-table/columns";
-import { DataTable } from "@/components/custom/absenteeism-report-table/data-table";
-import SummaryCard from "@/components/custom/summary-card";
-import { fetchAbsenteeismReport } from "@/lib/fetchers";
+import DashboardSummaryCards from "@/components/custom/dashboard-summary-cards";
+import { DataTableSkeleton } from "@/components/skeleton/data-table-skeleton";
+import PayrollSummaryCardsSkeleton from "@/components/skeleton/payroll-summary-cards-skeleton";
+import { Suspense } from "react";
 
-export default async function DashboardPage() {
-  const data = await fetchAbsenteeismReport("monthly").catch((err) => {
-    console.error(err);
-    return [];
-  });
+export default async function DashboardPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const period =
+    ((await searchParams).period as unknown as
+      | "quarterly"
+      | "yearly"
+      | "monthly") ?? "monthly";
+
   return (
     <div className="flex flex-col items-center justify-start min-h-screen px-8 pt-2 pb-20 gap-4 w-full">
       <div className="flex items-center justify-between w-full gap-2">
         <h1 className="text-2xl font-bold">Dashboard</h1>
       </div>
-      <div className="flex items-center justify-center w-full h-full gap-4">
-        <SummaryCard description="Total Employees" value={100} type="number" />
-        <SummaryCard description="Total Departments" value={5} type="number" />
-        <SummaryCard description="Total Positions" value={10} type="number" />
-        <SummaryCard
-          description="Estimated Monthly Payroll"
-          value={100000}
-          type="currency"
-        />
-      </div>
-      <div className="flex items-center justify-center w-full h-full gap-4">
-        <DataTable columns={columns} data={data} />
+      <Suspense fallback={<PayrollSummaryCardsSkeleton />}>
+        <DashboardSummaryCards />
+      </Suspense>
+      <div className="flex flex-col items-center justify-between w-full gap-2">
+        <div className="flex items-center justify-between w-full gap-2">
+          <h2 className="text-xl font-bold">Absenteeism Report</h2>
+          <AbsenteeismPeriodSelect />
+        </div>
+        <div className="flex items-center justify-center w-full h-full gap-2">
+          <Suspense
+            fallback={
+              <DataTableSkeleton columns={columns} data={[]} rows={10} />
+            }
+          >
+            <AbsenteeismReportTableContainer period={period} />
+          </Suspense>
+        </div>
       </div>
     </div>
   );
